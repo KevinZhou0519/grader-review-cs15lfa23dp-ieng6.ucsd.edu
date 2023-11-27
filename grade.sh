8519d85de18bda8236036e618fab36ac497119cd
KevinZhou0519 <101943628+KevinZhou0519@users.noreply.github.com>
2023-11-27 13:06:56 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+set -e
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,7 +9,27 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -e 'student-submission/ListExamples.java' ]]
+then
+    echo "File exist"
+else
+    echo "wrong file submitted"
+fi
 
+cp -r student-submission/ListExamples.java grading-area
+cp -r TestListExamples.java grading-area
+cd grading-area
+
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "compile success"
+    java -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" org.junit.runner.JUnitCore ListExamples.java
+    
+else
+    echo "compile error!!!"
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
